ce7086c1579b85f7f7a93c1653b39929b5e151a6
atulabhi <atulabhi@gmail.com>
2021-09-09 13:12:30 +0530

    fix script
    Signed-off-by: atulabhi <atulabhi@gmail.com>

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -x 
+set -x
 
 REPO=$1
 TAG=$2
 # Install dgoss
-curl -L https://raw.githubusercontent.com/aelsabbahy/goss/master/extras/dgoss/dgoss -o ./dgoss
-chmod +rx ./dgoss
+curl -L https://raw.githubusercontent.com/aelsabbahy/goss/master/extras/dgoss/dgoss -o /usr/local/bin/dgoss
+chmod +rx /usr/local/bin/dgoss
 
 # Install goss
 curl -L https://github.com/aelsabbahy/goss/releases/download/v0.3.6/goss-linux-amd64 -o ./goss
 ls
 export GOSS_PATH=./goss
 echo $GOSS_PATH
-dgoss run --hostname worker-1 $REPO:$TAG
+"./dgoss run --hostname worker-1 $REPO:$TAG"
