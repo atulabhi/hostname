97637aa7646b930734543d11bafd9991a187db90
atulabhi <atulabhi@gmail.com>
2021-09-09 12:59:35 +0530

    fix script
    Signed-off-by: atulabhi <atulabhi@gmail.com>

--- test.sh
+++ test.sh
@@ -9,5 +9,7 @@ chmod +rx ./dgoss
 
 # Install goss
 curl -L https://github.com/aelsabbahy/goss/releases/download/v0.3.6/goss-linux-amd64 -o ./goss
+ls
 export GOSS_PATH=./goss
+echo $GOSS_PATH
 dgoss run --hostname worker-1 $REPO:$TAG
