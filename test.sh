ca174c25ca8bd901113b1e68545359ad1d0c69c5
atulabhi <atulabhi@gmail.com>
2021-09-09 13:39:02 +0530

    fix script
    Signed-off-by: atulabhi <atulabhi@gmail.com>

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ REPO=$1
 TAG=$2
 # Install dgoss
 curl -L https://raw.githubusercontent.com/aelsabbahy/goss/master/extras/dgoss/dgoss
-chmod +rx dgoss && sudo mv /usr/local/bin/dgoss
+chmod +rx ./dgoss && sudo mv /usr/local/bin/dgoss
 
 # Install goss
 curl -L https://github.com/aelsabbahy/goss/releases/download/v0.3.6/goss-linux-amd64 -o ./goss
